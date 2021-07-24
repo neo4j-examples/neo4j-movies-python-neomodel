89b652ec866ebaa8422838e28ee1457b120050cd
Cristina <ce@tsl.io>
2021-07-24 13:10:43 -0400

    added warning

--- movies/admin.py
+++ movies/admin.py
@@ -3,6 +3,8 @@ from django_neomodel import admin as neo_admin
 
 from .models import Movie, Person
 
+# warning! If you're using Sandbox you'll have to add uuids
+
 class MovieAdmin(dj_admin.ModelAdmin):
     list_display = ("title","uuid")
 neo_admin.register(Movie, MovieAdmin)
